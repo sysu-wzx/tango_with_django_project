c13561d4e83bea28ac2b38e9e952440d7c3d887c
Zixiao Wang <2928605w@student.gla.ac.uk>
2024-01-23 19:16:14 +0000

    chapter 3

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return render(request, 'rango/index.html')
+def about(request):
+	return render(request, 'rango/about.html')
